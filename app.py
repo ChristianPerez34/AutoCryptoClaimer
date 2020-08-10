54820df8f56ee69b030e102cf00711da158fd444
Christian Pérez Villanueva <perez.villanueva.christian34@gmail.com>
2020-08-10 18:56:27 -0400

    Scheduler runs app every hour

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import pathlib
 import sys
+import time
+
+from apscheduler.schedulers.background import BackgroundScheduler
 
 from faucet_collector.crypto_claimer import CryptoClaimer
+
+
+def claim_crypto_job():
+    crypto_claimer = CryptoClaimer()
+    crypto_claimer.collect_crypto_faucets(file_path)
+    crypto_claimer.start_collecting_crypto()
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(claim_crypto_job, "interval", hours=1)
 
 try:
     file_path = pathlib.Path(sys.argv[1])
@@ -10,9 +23,13 @@ try:
         raise SystemExit(
             f"Unsupported file type: {file_path.suffix}\nSupported file type: .txt"
         )
+    scheduler.start()
 
-    crypto_claimer = CryptoClaimer()
-    crypto_claimer.collect_crypto_faucets(file_path)
-    crypto_claimer.start_collecting_crypto()
+    while True:
+        time.sleep(2)
+
+
 except IndexError:
     raise SystemExit(f"Usage: {sys.argv[0]} <file_to_read>")
+except (KeyboardInterrupt, SystemExit):
+    scheduler.shutdown()
--- faucet_collector/crypto_claimer.py
+++ faucet_collector/crypto_claimer.py
@@ -17,8 +17,9 @@ class CryptoClaimer:
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("window-size=1920,1080")
+        chrome_options.add_argument("--log-level=3")
 
-        self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        self.driver = webdriver.Chrome(chrome_options=chrome_options, service_log_path="NUL")
         self.driver.maximize_window()
 
         for url in self.crypto_faucets:
@@ -155,4 +156,4 @@ class CryptoClaimer:
                 .text
             )
 
-        print(f"Current balance: {balance}")
+        print(f"Current balance: {balance}\n")
